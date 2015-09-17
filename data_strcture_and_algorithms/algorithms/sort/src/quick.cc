6158f36ae803f0e03be065b2eb472b03c2dec1fc
Chao Mai <loneymai@gmail.com>
2015-09-17 15:19:45 +0800

    finish sort

--- data_strcture_and_algorithms/algorithms/sort/src/quick.cc
+++ data_strcture_and_algorithms/algorithms/sort/src/quick.cc
@@ -1,3 +1,58 @@
+//                                                               +----->pivot
+//               ij                                              +
+//                                                               x
+//                +----------------------------------------------+
+//                p                                              r
+//
+//
+//
+//                         i                    j                x
+//                +----------------------------------------------+
+//                p                                              r
+//                +---+----+
+//                    |
+//                    v     +--------+----------+
+//                 (<= x)            |
+//                                   v
+//                                 (> x)
+//
+//                                                               j
+//                                    i                          x
+// end of loop:   +----------------------------------------------+
+//                 p                                             r
+//
+//                                     +------------+------------+
+//                                                  |
+//                                                  v
+//                                            swap a[i+1] and a[r]
+
 #include "quick.h"
 
-void quick_sort(int arr[], int n) {}
+int q_partition(int arr[], int p, int r);
+
+void q_sort(int arr[], int p, int r);
+
+void quick_sort(int arr[], int n) { q_sort(arr, 0, n - 1); }
+
+int q_partition(int arr[], int p, int r) {
+  int x = arr[r];
+  int i = p - 1;
+
+  for (int j = p; j < r; ++j) {
+    if (arr[j] <= x) {
+      ++i;
+      swap(&arr[i], &arr[j]);
+    }
+  }
+  swap(&arr[i + 1], &arr[r]);
+
+  return i + 1;
+}
+
+void q_sort(int arr[], int p, int r) {
+  if (p < r) {
+    int q = q_partition(arr, p, r);
+    q_sort(arr, p, q - 1);
+    q_sort(arr, q + 1, r);
+  }
+}
--- data_strcture_and_algorithms/algorithms/sort/src/quick.h
+++ data_strcture_and_algorithms/algorithms/sort/src/quick.h
@@ -1,6 +1,8 @@
 #ifndef QUICK_H_
 #define QUICK_H_
 
+#include "utilities.h"
+
 void quick_sort(int arr[], int n);
 
 #endif
