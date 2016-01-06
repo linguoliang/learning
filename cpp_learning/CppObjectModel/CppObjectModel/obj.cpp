98cbf6e88d5c649108599cb69f3fb966f650cbbd
Chao Mai <loneymai@gmail.com>
2016-01-06 22:18:39 +0800

    update

--- cpp_learning/CppObjectModel/CppObjectModel/obj.cpp
+++ cpp_learning/CppObjectModel/CppObjectModel/obj.cpp
@@ -9,6 +9,7 @@ using std::endl;
 
 void Test1();
 void Test2();
+void Test3();
 
 class Parent
 {
@@ -74,9 +75,50 @@ public:
   virtual void g1() { cout << "Derive::g1()" << endl; }
 };
 
+class B {
+public:
+  int ib;
+  char cb;
+  B() :ib(0), cb('B') {}
+  virtual void f() { cout << "B::f()" << endl; }
+  virtual void Bf() { cout << "B::Bf()" << endl; }
+};
+
+class B1 : public B {
+public:
+  int ib1;
+  char cb1;
+  B1() :ib1(11), cb1('1') {}
+  virtual void f() { cout << "B1::f()" << endl; }
+  virtual void f1() { cout << "B1::f1()" << endl; }
+  virtual void Bf1() { cout << "B1::Bf1()" << endl; }
+};
+
+class B2 : public B {
+public:
+  int ib2;
+  char cb2;
+  B2() :ib2(12), cb2('2') {}
+  virtual void f() { cout << "B2::f()" << endl; }
+  virtual void f2() { cout << "B2::f2()" << endl; }
+  virtual void Bf2() { cout << "B2::Bf2()" << endl; }
+};
+
+class D : public B1, public B2 {
+public:
+  int id;
+  char cd;
+  D() :id(100), cd('D') {}
+  virtual void f() { cout << "D::f()" << endl; }
+  virtual void f1() { cout << "D::f1()" << endl; }
+  virtual void f2() { cout << "D::f2()" << endl; }
+  virtual void Df() { cout << "D::Df()" << endl; }
+};
+
 void Obj() {
   Test1();
   Test2();
+  Test3();
 }
 
 void Test1() {
@@ -99,13 +141,36 @@ void Test2() {
   ((void(*)(void))(((int*)(*(int*)&d))[2]))();
   ((void(*)(void))(((int*)(*(int*)&d))[3]))();
 
+  cout << endl;
+
   int s = sizeof(Base1) / 4;
   ((void(*)(void))((int*)(((int*)&d)[s]))[0])();
   ((void(*)(void))((int*)(((int*)&d)[s]))[1])();
   ((void(*)(void))((int*)(((int*)&d)[s]))[2])();
 
+  cout << endl;
+
   s += sizeof(Base2) / 4;
   ((void(*)(void))((int*)(((int*)&d)[s]))[0])();
   ((void(*)(void))((int*)(((int*)&d)[s]))[1])();
   ((void(*)(void))((int*)(((int*)&d)[s]))[2])();
+}
+
+void Test3() {
+  cout << endl << "Test3" << endl;
+
+  D d;
+  ((void(*)(void))(((int*)(*(int*)&d))[0]))();
+  ((void(*)(void))(((int*)(*(int*)&d))[1]))();
+  ((void(*)(void))(((int*)(*(int*)&d))[2]))();
+  ((void(*)(void))(((int*)(*(int*)&d))[3]))();
+  ((void(*)(void))(((int*)(*(int*)&d))[4]))();
+
+  cout << endl;
+
+  int s = sizeof(B1) / 4;
+  ((void(*)(void))((int*)(((int*)&d)[s]))[0])();
+  ((void(*)(void))((int*)(((int*)&d)[s]))[1])();
+  ((void(*)(void))((int*)(((int*)&d)[s]))[2])();
+  ((void(*)(void))((int*)(((int*)&d)[s]))[3])();
 }
